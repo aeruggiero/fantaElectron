e30a2cf345826406a13b66582de423d820efb322
Angelj107 <jordangelx@gmail.com>
2023-07-22 16:35:37 +0200

    AAAAAAAAA

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,7 +1,6 @@
-import type {ElectronApplication, JSHandle} from 'playwright';
+import type {ElectronApplication} from 'playwright';
 import {_electron as electron} from 'playwright';
 import {afterAll, beforeAll, expect, test} from 'vitest';
-import type {BrowserWindow} from 'electron';
 
 let electronApp: ElectronApplication;
 
@@ -11,61 +10,6 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await electronApp.close();
-});
-
-test('Main window state', async () => {
-  const page = await electronApp.firstWindow();
-  const window: JSHandle<BrowserWindow> = await electronApp.browserWindow(page);
-  const windowState = await window.evaluate(
-    (mainWindow): Promise<{isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean}> => {
-      const getState = () => ({
-        isVisible: mainWindow.isVisible(),
-        isDevToolsOpened: mainWindow.webContents.isDevToolsOpened(),
-        isCrashed: mainWindow.webContents.isCrashed(),
-      });
-
-      return new Promise(resolve => {
-        /**
-         * The main window is created hidden, and is shown only when it is ready.
-         * See {@link ../packages/main/src/mainWindow.ts} function
-         */
-        if (mainWindow.isVisible()) {
-          resolve(getState());
-        } else mainWindow.once('ready-to-show', () => resolve(getState()));
-      });
-    },
-  );
-
-  expect(windowState.isCrashed, 'The app has crashed').toBeFalsy();
-  expect(windowState.isVisible, 'The main window was not visible').toBeTruthy();
-  expect(windowState.isDevToolsOpened, 'The DevTools panel was open').toBeFalsy();
-});
-
-test('Main window web content', async () => {
-  const page = await electronApp.firstWindow();
-  const element = await page.$('#app', {strict: true});
-  expect(element, 'Was unable to find the root element').toBeDefined();
-  expect((await element.innerHTML()).trim(), 'Window content was empty').not.equal('');
-});
-
-test('Preload versions', async () => {
-  const page = await electronApp.firstWindow();
-  const versionsElement = page.locator('#process-versions');
-  expect(await versionsElement.count(), 'expect find one element #process-versions').toStrictEqual(
-    1,
-  );
-
-  /**
-   * In this test we check only text value and don't care about formatting. That's why here we remove any space symbols
-   */
-  const renderedVersions = (await versionsElement.innerText()).replace(/\s/g, '');
-  const expectedVersions = await electronApp.evaluate(() => process.versions);
-
-  for (const expectedVersionsKey in expectedVersions) {
-    expect(renderedVersions).include(
-      `${expectedVersionsKey}:v${expectedVersions[expectedVersionsKey]}`,
-    );
-  }
 });
 
 test('Preload nodeCrypto', async () => {
