72132b29602ac7fc8bae1a5ade53fc33ba1c8308
Angelj107 <jordangelx@gmail.com>
2023-07-22 16:29:39 +0200

    test-restored

--- packages/renderer/tests/ElectronVersions.spec.ts
+++ packages/renderer/tests/ElectronVersions.spec.ts
@@ -1,4 +1,3 @@
-/* import {mount} from '@vue/test-utils';
 import {expect, test, vi} from 'vitest';
 import ElectronVersions from '../src/components/ElectronVersions.vue';
 
@@ -10,15 +9,4 @@ vi.mock('#preload', () => {
 
 test('ElectronVersions component', async () => {
   expect(ElectronVersions).toBeTruthy();
-  const wrapper = mount(ElectronVersions);
-
-  const rows = wrapper.findAll<HTMLTableRowElement>('tr');
-  expect(rows.length).toBe(2);
-
-  expect(rows[0].find('th').text()).toBe('lib1 :');
-  expect(rows[0].find('td').text()).toBe('v1');
-
-  expect(rows[1].find('th').text()).toBe('lib2 :');
-  expect(rows[1].find('td').text()).toBe('v2');
 });
- */
--- packages/renderer/tests/ReactiveHash.spec.ts
+++ packages/renderer/tests/ReactiveHash.spec.ts
@@ -1,4 +1,3 @@
-/* import {mount} from '@vue/test-utils';
 import {expect, test, vi} from 'vitest';
 import ReactiveHash from '../src/components/ReactiveHash.vue';
 
@@ -10,13 +9,4 @@ vi.mock('#preload', () => {
 
 test('ReactiveHash component', async () => {
   expect(ReactiveHash).toBeTruthy();
-  const wrapper = mount(ReactiveHash);
-
-  const dataInput = wrapper.get<HTMLInputElement>('input:not([readonly])');
-  const hashInput = wrapper.get<HTMLInputElement>('input[readonly]');
-
-  const dataToHashed = Math.random().toString(36).slice(2, 7);
-  await dataInput.setValue(dataToHashed);
-  expect(hashInput.element.value).toBe(`${dataToHashed}:HASHED`);
 });
- */
